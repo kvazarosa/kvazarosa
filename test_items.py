afde3040fc109ae534e19c29e8d3ed477174424c
kvazarosa <160460983+kvazarosa@users.noreply.github.com>
2024-12-04 12:19:20 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ import time
 def test_button_is_present(browser):
     url = f"http://selenium1py.pythonanywhere.com/{browser.language}/catalogue/coders-at-work_207/"
     browser.get(url)
-    time.sleep(15)
+    time.sleep(30)
 
     button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(button) > 0, '!!!Кнопка не найдена!!!!'
