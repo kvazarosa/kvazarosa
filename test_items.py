fa121dbc7301d102b06e1351c46edca1859c4ea4
kvazarosa <160460983+kvazarosa@users.noreply.github.com>
2024-12-04 12:15:52 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import time
+
+
+# Тестовая функция, которая принимает браузер как аргумент
+def test_button_is_present(browser):
+    url = f"http://selenium1py.pythonanywhere.com/{browser.language}/catalogue/coders-at-work_207/"
+    browser.get(url)
+    time.sleep(15)
+
+    # Проверяем наличие кнопки добавления в корзину
+    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(button) > 0, '!!!Кнопка не найдена!!!!'
 
